0f1fd09b57fb0037beb5df0f9e4977b5614beee9
carlos-celfo <cagelvis.pt@gmail.com>
2022-03-02 18:54:38 +0000

    Adding the possibility to write in the notes

--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ class App extends Component {
         id: Date.now(),
         title: "",
         description: "",
-        doesMatchSearch: true,
-      },
+        doesMatchSearch: true
+      }
     ],
-    searchText: "",
+    searchText: ""
   };
 
   addNote = () => {
@@ -20,20 +20,38 @@ class App extends Component {
       id: Date.now(),
       title: "",
       description: "",
-      doesMatchSearch: true,
+      doesMatchSearch: true
     };
 
     this.setState({ notes: [newNote, ...this.state.notes] });
+  };
+
+  onType = (editMeId, updatedKey, updatedValue) => {
+    const updatedNotes = this.state.notes.map((note) => {
+      if (note.id !== editMeId) {
+        return note;
+      } else {
+        if (updatedKey === "title") {
+          note.title = updatedValue;
+          return note;
+        } else {
+          note.description = updatedValue;
+          return note;
+        }
+      }
+    });
+    this.setState({ notes: updatedNotes });
   };
 
   render() {
     return (
       <div>
         <Header addNote={this.addNote} searchText={this.state.searchText} />
-        <NotesList notes={this.state.notes} />
+        <NotesList onType={this.onType} notes={this.state.notes} />
       </div>
     );
   }
 }
 
+export default App;
 export default App;
--- src/NotesList.js
+++ src/NotesList.js
@@ -2,10 +2,12 @@ import React from "react";
 import Note from "./Note.js";
 
 const NotesList = (props) => {
-  const keepSearchMatches = (note) => note.doesSearchMatch;
+  const keepSearchMatches = (note) => note.doesMatchSearch;
   const filteredSearch = props.notes.filter(keepSearchMatches);
 
-  const renderNote = (note) => <Note note={note} key={note.id} />;
+  const renderNote = (note) => (
+    <Note onType={props.onType} note={note} key={note.id} />
+  );
   const noteElements = filteredSearch.map(renderNote);
   return <ul className="notes-list">{noteElements}</ul>;
 };
