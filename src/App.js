24e7e5122c94461919a9076e187909895b2a6bc5
carlos-celfo <cagelvis.pt@gmail.com>
2022-03-02 15:30:17 +0000

    Updated app.js

--- src/App.js
+++ src/App.js
@@ -29,8 +29,8 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Header />
-        <NotesList />
+        <Header addNote={this.addNote} searchText={this.state.searchText} />
+        <NotesList notes={this.state.notes} />
       </div>
     );
   }
